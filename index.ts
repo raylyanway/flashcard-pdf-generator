c4e6e17300f76a4c0b1c02a0d87d1c5feb68ed98
iLia Railian <raylyanway@mail.ru>
2026-03-29 02:12:58 +0000

    udpate rendering group

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import * as cards from './cards';
 import renderCards from './utils';
 
 const { numbersA1, colorsA1, ...rest } = cards;
-const A1 = { ...numbersA1, ...colorsA1 };
+const A1 = [...numbersA1, ...colorsA1];
 
 renderCards({ A1, ...rest });
